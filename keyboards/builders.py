4f926edef2130ed279007b026adabdd5075b4ca5
1ustaa <sergey00rus24680@gmail.com>
2025-04-17 23:22:14 +0300

    Добавил функцию для создания пагинации

--- keyboards/builders.py
+++ keyboards/builders.py
@@ -3,7 +3,7 @@ from data.model import session, Categories, Manufacturers, Models, manufacturer_
 from keyboards.inline import main_menu_button, back_button
 from sqlalchemy import desc
 
-MAX_PAGE_SIZE = 4
+MAX_PAGE_SIZE = 2
 
 def categories_kb(page=0 ,page_size: int=MAX_PAGE_SIZE):
     categories_count = session.query(Categories).count()
@@ -16,24 +16,27 @@ def categories_kb(page=0 ,page_size: int=MAX_PAGE_SIZE):
     [builder.button(text=category.name, callback_data=f"category_{category.id}") for category in categories]
     builder.adjust(2)
 
-    pagination_buttons = []
-    if total_pages > 1:
-        prev_page = (page - 1) if page > 0 else total_pages - 1
-        next_page = (page + 1) % total_pages
+    # pagination_buttons = []
+    # if total_pages > 1:
+    #     prev_page = (page - 1) if page > 0 else total_pages - 1
+    #     next_page = (page + 1) % total_pages
+    #
+    #     pagination_buttons.append(
+    #         InlineKeyboardButton(
+    #             text="⬅️",
+    #             callback_data=f"pg_category_prev_{prev_page}"
+    #         )
+    #     )
+    #     pagination_buttons.append(
+    #         InlineKeyboardButton(
+    #             text="➡️",
+    #             callback_data=f"pg_category_next_{next_page}"
+    #         )
+    #     )
+    #     builder.row(*pagination_buttons)
 
-        pagination_buttons.append(
-            InlineKeyboardButton(
-                text="⬅️",
-                callback_data=f"pg_category_prev_{prev_page}"
-            )
-        )
-        pagination_buttons.append(
-            InlineKeyboardButton(
-                text="➡️",
-                callback_data=f"pg_category_next_{next_page}"
-            )
-        )
-        builder.row(*pagination_buttons)
+    pg_buttons = make_pagination_buttons("pg_category", [], total_pages, page)
+    builder.row(*pg_buttons)
 
     builder.row(back_button())
     builder.row(main_menu_button())
@@ -54,24 +57,8 @@ def manufacturer_kb(category_id, page = 0, page_size: int=MAX_PAGE_SIZE):
         builder.button(text=manufacturer.name, callback_data=f"manufacturer_{manufacturer.id}")
     builder.adjust(2)
 
-    pagination_buttons = []
-    if total_pages > 1:
-        prev_page = (page - 1) if page > 0 else total_pages - 1
-        next_page = (page + 1) % total_pages
-
-        pagination_buttons.append(
-            InlineKeyboardButton(
-                text="⬅️",
-                callback_data=f"pg_manufacturer_prev_{category_id}_{prev_page}"
-            )
-        )
-        pagination_buttons.append(
-            InlineKeyboardButton(
-                text="➡️",
-                callback_data=f"pg_manufacturer_next_{category_id}_{next_page}"
-            )
-        )
-        builder.row(*pagination_buttons)
+    pg_buttons = make_pagination_buttons("pg_manufacturer", [category_id], total_pages, page)
+    builder.row(*pg_buttons)
 
     builder.row(back_button())
     builder.row(main_menu_button())
@@ -90,24 +77,8 @@ def models_kb(category_id: int, manufacturer_id: int, page: int = 0, page_size: 
         builder.button(text=model.name, callback_data=f"model_{model.id}")
     builder.adjust(2)
 
-    pagination_buttons = []
-    if total_pages > 1:
-        prev_page = (page - 1) if page > 0 else total_pages - 1
-        next_page = (page + 1) % total_pages
-
-        pagination_buttons.append(
-            InlineKeyboardButton(
-                text="⬅️",
-                callback_data=f"pg_model_prev_{category_id}_{manufacturer_id}_{prev_page}"
-            )
-        )
-        pagination_buttons.append(
-            InlineKeyboardButton(
-                text="➡️",
-                callback_data=f"pg_model_next_{category_id}_{manufacturer_id}_{next_page}"
-            )
-        )
-        builder.row(*pagination_buttons)
+    pg_buttons = make_pagination_buttons("pg_model", [category_id, manufacturer_id], total_pages, page)
+    builder.row(*pg_buttons)
 
     builder.row(back_button())
     builder.row(main_menu_button())
@@ -141,6 +112,27 @@ def colors_kb(model_id, page: int = 0, page_size: int=MAX_PAGE_SIZE):
         builder.button(text=color.name, callback_data=f"color_{color.id}")
     builder.adjust(2)
 
+    pg_buttons = make_pagination_buttons("pg_color",[model_id], total_pages, page)
+    builder.row(*pg_buttons)
+
+    builder.row(back_button())
+    builder.row(main_menu_button())
+    return builder.as_markup()
+
+def make_pagination_buttons(prefix: str, items: list, total_pages: int, page: int) -> list:
+    """
+    :param prefix: str — префикс для callback_data (например, 'pg_model')
+    :param items: list — список данных, включаемых в callback_data (например: [category_id, manufacturer_id, ...])
+    :param total_pages: int — общее количество страниц
+    :param page: int — текущая страница
+    :return: list — список из InlineKeyboardButton'ов (⬅️ и ➡️ при необходимости)
+    """
+
+    if items:
+        callback_data_string = "_" + "_".join(str(item) for item in items)
+    else:
+        callback_data_string = ""
+
     pagination_buttons = []
     if total_pages > 1:
         prev_page = (page - 1) if page > 0 else total_pages - 1
@@ -149,17 +141,13 @@ def colors_kb(model_id, page: int = 0, page_size: int=MAX_PAGE_SIZE):
         pagination_buttons.append(
             InlineKeyboardButton(
                 text="⬅️",
-                callback_data=f"pg_color_prev_{model_id}_{prev_page}"
+                callback_data = f"{prefix}_prev{callback_data_string}_{prev_page}"
             )
         )
         pagination_buttons.append(
             InlineKeyboardButton(
                 text="➡️",
-                callback_data=f"pg_color_next_{model_id}_{next_page}"
+                callback_data = f"{prefix}_next{callback_data_string}_{next_page}"
             )
         )
-        builder.row(*pagination_buttons)
-
-    builder.row(back_button())
-    builder.row(main_menu_button())
-    return builder.as_markup()
+    return pagination_buttons
